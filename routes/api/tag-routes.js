7d71920054816d4e1152287f997ea89cb09378da
Joshua Ellis <jjellis1111@gmail.com>
2022-02-28 21:30:54 -0500

    Fixed tags u, Need to fix products c

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -48,9 +48,10 @@ router.post('/', async (req, res) => {
 router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
   try {
-    const tagData = await Tag.update({
-      where: { id: req.params.id }
-    });
+    const tagData = await Tag.update(req.body, {
+      where: {id: req.params.id,},
+      tag_name: req.body.tag_name
+    })
     if (!tagData) {
       res.status(404).json({ message: 'No tag with this id!' });
       return;
