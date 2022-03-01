96f8549faef3c81c52cdf914325dba12207be870
Joshua Ellis <jjellis1111@gmail.com>
2022-02-28 21:22:02 -0500

    Fixed category u, Need to fix products c&u / tags u

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -49,6 +49,8 @@ router.put('/:id', async (req, res) => {
   // update a category by its `id` value
   try {
     const categoryData = await Category.update({
+      category_name: req.body.category_name
+    },{
       where: { id: req.params.id }
     });
     if (!categoryData) {
